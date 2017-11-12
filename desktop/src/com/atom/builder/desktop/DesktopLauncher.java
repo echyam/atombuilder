daf2e527e7df4cc9ee036bb06955f1e240067ead
echyam <echyam@gmail.com>
2017-11-12 18:18:24 +0100

    draw shapes that move

--- desktop/src/com/atom/builder/desktop/DesktopLauncher.java
+++ desktop/src/com/atom/builder/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.atom.builder.AtomGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "atomgame";
+		config.width = 1600;
+		config.height = 900;
 		new LwjglApplication(new AtomGame(), config);
 	}
 }
