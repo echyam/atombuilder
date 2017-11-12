ad6255828070cf293b547a9a92fd7825fc6b5dd5
echyam <echyam@gmail.com>
2017-11-12 15:48:03 +0100

    initial commit

---
+++ android/src/com/atom/builder/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.atom.builder;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.atom.builder.AtomGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AtomGame(), config);
+	}
+}
---
+++ desktop/src/com/atom/builder/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.atom.builder.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.atom.builder.AtomGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AtomGame(), config);
+	}
+}
